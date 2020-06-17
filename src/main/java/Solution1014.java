7d788c0f71178ce8ada3f4748a82fbd5a4e4d1a1
u1095279207515 <1095279207515@tianchi.com>
2020-06-17 23:28:42 +0800

    1014 1431 1470 1480

---
+++ src/main/java/Solution1014.java
@@ -0,0 +1,51 @@
+public class Solution1014 {
+    /**
+     给定正整数数组 A，A[i] 表示第 i 个观光景点的评分，并且两个景点 i 和 j 之间的距离为 j - i。
+     一对景点（i < j）组成的观光组合的得分为（A[i] + A[j] + i - j）：景点的评分之和减去它们两者之间的距离。
+
+     返回一对观光景点能取得的最高分。
+
+     来源：力扣（LeetCode）
+     链接：https://leetcode-cn.com/problems/best-sightseeing-pair
+     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param A
+     * @return
+     */
+    public static int maxScoreSightseeingPair(int[] A) {
+        //暴力解法 超出时间限制
+        int maxScore = 0;
+        for (int i=0; i<A.length-1; i++) {
+            for (int j=i+1; j<A.length; j++) {
+                int tmp = A[i]+A[j]+i-j;
+                if (tmp>maxScore) {
+                    maxScore = tmp;
+                }
+            }
+        }
+        return maxScore;
+    }
+
+    public static int maxScoreSightseeingPair2(int[] A) {
+        //1.保存每次j前面 A[i]+i的最大值preMax
+        //2.比较当前最大值 preMax+A[j]-j
+        int maxScore = 0;
+        int preMax = A[0];
+        for (int j=1; j<A.length; j++) {
+            if (preMax + A[j]-j > maxScore) {
+                maxScore = preMax + A[j]-j;
+            }
+            if (A[j]+j > preMax) {
+                preMax = A[j]+j;
+            }
+
+        }
+        return maxScore;
+    }
+
+    public static void main(String[] args) {
+        int[] test1 = {8,1,5,2,6};
+        System.out.println(Solution1014.maxScoreSightseeingPair(test1));
+        System.out.println(Solution1014.maxScoreSightseeingPair2(test1));
+    }
+
+}
---
+++ src/main/java/Solution1470.java
@@ -0,0 +1,35 @@
+public class Solution1470 {
+    public static int[] shuffle(int[] nums, int n) {
+        /**
+         * 给你一个数组 nums ，数组中有 2n 个元素，按 [x1,x2,...,xn,y1,y2,...,yn] 的格式排列。
+
+         请你将数组按 [x1,y1,x2,y2,...,xn,yn] 格式重新排列，返回重排后的数组。
+
+         来源：力扣（LeetCode）
+         链接：https://leetcode-cn.com/problems/shuffle-the-array
+         著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+         输入：nums = [2,5,1,3,4,7], n = 3
+         输出：[2,3,5,4,1,7]
+         解释：由于 x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 ，所以答案为 [2,3,5,4,1,7]
+
+         */
+        int[] res = new int[nums.length];
+        int mid = nums.length/2;
+        for (int i=0; i<mid; i++) {
+            res[i*2] = nums[i];
+            res[i*2+1] = nums[i+mid];
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {2,5,1,3,4,7};
+        int n = 3;
+        int[] res = Solution1470.shuffle(nums, n);
+        for (int i=0; i<res.length; i++) {
+            System.out.print(res[i] + " ");
+        }
+    }
+}
