ce9a4e2a7596967d967e5fc8f895b448218490fd
u1095279207515 <1095279207515@tianchi.com>
2020-06-28 08:20:39 +0800

    回溯

---
+++ src/main/java/BackTrack.java
@@ -0,0 +1,93 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class BackTrack {
+    /**
+     回溯算法基本框架：
+     result = []
+     def backtrack(路径, 选择列表):
+        if 满足结束条件:
+            result.add(路径)
+        return
+
+        for 选择 in 选择列表:
+            做选择
+            backtrack(路径, 选择列表)
+            撤销选择
+
+
+
+     */
+    List<List<Integer>> res = new LinkedList<List<Integer>>();
+    public List<List<Integer>> permute(int[] nums) {
+        //记录每一条路径
+        LinkedList<Integer> track = new LinkedList<Integer>();
+        backtrack(nums, track);
+        return res;
+    }
+    public void backtrack(int[] nums, LinkedList<Integer> track) {
+        if (track.size() == nums.length) {
+            res.add(new LinkedList<Integer>(track));
+            return;
+        }
+
+        for (int i=0; i<nums.length; i++) {
+            if (track.contains(nums[i])) continue;
+            //做选择
+            track.add(nums[i]);
+            // 进入下一个决策树
+            backtrack(nums, track);
+            //取消选择
+            track.removeLast();
+
+
+
+        }
+    }
+
+
+
+    //N皇后
+
+    /**
+     给你一个 N×N 的棋盘，让你放置 N 个皇后，使得它们不能互相攻击。
+     PS：皇后可以攻击同一行、同一列、左上左下右上右下四个方向的任意单位。
+     */
+    //n * n的棋盘
+    List<List<String>> ress = new LinkedList<List<String>>();
+    public List<List<String>> nQueen(int n) {
+        List<String> bord = new LinkedList<String>();
+        for (int i=0; i<n; i++) bord.add(".");
+        nQueenBackTrack(bord, 0);
+        return ress;
+    }
+
+    public void nQueenBackTrack(List<String> bord, int row) {
+        if (row == bord.size()) {
+            res.add(bord);
+            return;
+        }
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        //全排列 输入一组不重复的数字
+        int[] nums = {1,2,3};
+        BackTrack s = new BackTrack();
+        List<List<Integer>> r = s.permute(nums);
+        System.out.println(r);
+
+
+
+        //N皇后
+
+
+    }
+
+
+
+}
--- src/main/java/CoinCount.java
+++ src/main/java/CoinCount.java
@@ -20,12 +20,59 @@ public class CoinCount {
 
     }
 
+
+    public int coinChange2(int[] coins, int amount) {
+        //暴力递归
+        if (amount<=0) {
+            return 0;
+        }
+        int res = Integer.MAX_VALUE;
+        int[] mem = new int[amount+1];
+        return dp2(coins, amount, res, mem);
+    }
+
+    public int dp2(int[] coins, int n, int res, int[] mem) {
+        if (n==0) return 0;
+        if (n<0) return -1;
+        if (mem[n]>0) return mem[n];
+        for (int coin:coins) {
+            int subProblem = dp2(coins,n-coin, res, mem);
+            if (subProblem==-1) continue; //此问题无解
+            res = Math.min(res, 1+subProblem);
+        }
+        mem[n] = res==Integer.MAX_VALUE?-1:res;
+        return res!=Integer.MAX_VALUE? res:-1;
+
+    }
+
+    public int coinChange3(int[] coins, int amount) {
+        //动态规划
+        int[] dp = new int[amount+1];
+        for (int i=0; i<amount+1; i++) {
+            dp[i] = amount+1;
+        }
+        dp[0] = 0; //设置base case
+        for (int i=0; i<dp.length; i++) {
+            for (int coin:coins) {
+                if (i-coin<0) continue;
+                dp[i] = Math.min(dp[i], 1+dp[i-coin]);
+
+            }
+        }
+        return dp[amount]==amount+1?-1:dp[amount];
+    }
+
+
     public static void main(String[] args) {
         int[] coins = {1,2,5};
         int amount = 11;
 
         CoinCount cc = new CoinCount();
-        System.out.println(cc.coinChange(coins, amount)); //3
+        //System.out.println(cc.coinChange(coins, amount)); //3
+
+        //System.out.println(cc.coinChange2(coins, amount)); //3
+        System.out.println(cc.coinChange3(coins, amount)); //3
+
     }
 
 }
